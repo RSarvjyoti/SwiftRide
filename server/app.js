4abe728f397a0f495117c0368e76c82aa45c9c29
RSarvjyoti <sarvjyoti64@gmail.com>
2025-06-25 09:43:55 +0530

    basic server setup

---
+++ server/app.js
@@ -0,0 +1,13 @@
+const { config } = require("dotenv");
+config();
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Home route");
+});
+
+module.exports = app;
